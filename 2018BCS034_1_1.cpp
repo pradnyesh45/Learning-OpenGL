8f2616935b5ee9a1f63ca93b0ba72a0f849e1e60
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2020-11-10 12:27:09 +0530

    assignment_1_1

---
+++ 2018BCS034_1_1.cpp
@@ -0,0 +1,44 @@
+#include<GL/glut.h>
+
+float angle=45;
+
+
+void polySegment(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0,0.0,0.0);
+	glTranslatef(5.f, 5.f, 0.f);
+    glRotatef(45.f, 0.f, 0.f, 1.f);
+    glTranslatef(-5.f, -5.f, 0.f);
+	
+	glBegin(GL_POLYGON);
+
+	int A[]={3,1};
+	int B[]={5,1};
+	int C[]={5,2};
+	int D[]={3,2};
+	                                  
+	glVertex2iv(A);
+	glVertex2iv(B);
+	glVertex2iv(C);
+	glVertex2iv(D);                                      
+	glEnd();
+	
+	glFlush();
+}
+
+
+int main(int argc,char** argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(0,0);
+	glutInitWindowSize(600,600);
+	glutCreateWindow("Assignment-1");
+	glClearColor(1.0,1.0,1.0,0.0);
+
+	glMatrixMode(GL_MODELVIEW);  
+	gluOrtho2D (-10,10.0,-10,10.0);
+
+	glutDisplayFunc(polySegment);
+	glutMainLoop();
+}
